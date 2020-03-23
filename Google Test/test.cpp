65eb3bf52908ededa9b87b2ab16469d83d352957
DAVID BENGTSSON <db96@live.se>
2020-03-23 14:54:13 +0100

    test3

--- Google Test/test.cpp
+++ Google Test/test.cpp
@@ -6,10 +6,26 @@
 Vector v = Vector(5.0f, 7.0f, 1.0f);
 int a = 1;
 
-TEST(TestCaseName, TestName) {
+TEST(VectorTests, LearningToTest) {
 
 
   EXPECT_EQ(a, 1);
   EXPECT_TRUE(true);
   EXPECT_FLOAT_EQ((int)8.6023f,(int)v.Length());
+}
+
+TEST(VectorTests, LearningToTest2) {
+
+
+	EXPECT_EQ(a, 1);
+	EXPECT_TRUE(true);
+	EXPECT_FLOAT_EQ((int)8.6023f, (int)v.Length());
+}
+
+TEST(VectorTests, LearningToTest3) {
+
+
+	EXPECT_EQ(a, 1);
+	EXPECT_TRUE(true);
+	EXPECT_FLOAT_EQ((int)8.6023f, (int)v.Length());
 }
